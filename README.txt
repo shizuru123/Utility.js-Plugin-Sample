b26202238ac28203438a464cbe43359964a5a800
Tashiro Shizuru <shizuru123@inter7.jp>
2012-10-22 19:30:55 +0900

    main file

---
+++ README.txt
@@ -0,0 +1,9 @@
+こんにちは。
+
+これはUtility.jsのプラグインのベースです。
+
+連絡先：shizuru123@inter7.jp
+
+使い方
+
+ダウンロードして、Utility_minisdk.jsをひらいて、PluginBaseクラスを継承して、Utility.jsの機能をうまく使ったプラグインをつくってください。
---
+++ utility_minisdk.js
@@ -0,0 +1,6 @@
+PluginBase = function(name, mode, author){
+	this.name = name;
+	this.progress = mode;
+	this.author = author;
+	console.log("Powered By Utility.js and Utility.js Plugin");
+};
